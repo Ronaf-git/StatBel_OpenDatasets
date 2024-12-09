31f6d7b2c438bb9e7c0768592d0583f177626af9
Ronaf <boris.depelsenaire@hotmail.com>
2024-12-09 21:36:27 +0100

    v0.1
    Commit v0.1

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# StatBel_OpenDatasets
-Get All Datasets existing in StatBel
+# StatBel Open Datasets
+
+**Author:** Ronaf (Ronaf-Git)  
+**Project:** [StatBel Open Datasets](https://github.com/Ronaf-git/StatBel_OpenDatasets)
+
+## Overview
+
+This tool allows users to interact with the [StatBel](https://statbel.fgov.be/en/statistics/bestat/faq) API to retrieve and export open datasets (referred to as "views") provided by the Belgian Statistical Office. You can export the data in multiple formats (CSV, XML, JSON, HTML, PDF, XLS) and choose specific views based on language and format preferences.
+
+The application also allows you to export metadata about datasets and view relationships between datasources and views.
+
+## Features
+
+- **Retrieve Metadata**: Export metadata about datasets (datasources and views) as text files.
+- **Export All Views**: Export all available datasets in multiple formats. Will take a long time. Not recommended.
+- **Filter by Language**: Export datasets in different languages (French, English, Dutch, German) - if available.
+- **Export Specific Views**: Select specific views to export based on chosen languages and formats.
+- **Progress Tracking**: View export progress in real-time with a progress bar.
+
+## How to Use
+
+### Prerequisites
+
+- **Windows OS**: The application is packaged as an executable (`.exe`), so no additional setup is required.
+- **Internet connection**: The application needs internet connection to get datas.
+- **No Installation Required**: Just run the `.exe` file on your system.
+
+### Running the Application
+
+1. **Download the Executable**: Download the executable file from the releases section of this repository.
+2. **Launch the Application**: Double-click the `.exe` file to launch the tool.
+
+### Main Features in the GUI
+
+- **Tab 1: About**  
+  Displays general information about the tool and instructions for usage.
+
+- **Tab 2: Metadata**  
+  - Export metadata about datasets (datasources and views).
+  - Show relationships between datasources and views.
+
+- **Tab 3: Export all Views**  
+  - Export all available views in all available format. Not recommended.
+
+- **Tab 4: Export specific Views**  
+  - Allows users to select specific views and export them in the desired format and language.
+  - Export progress is displayed in a progress bar.
+
+### Available Formats
+
+- **XML**
+- **JSON**
+- **CSV**
+- **XLS**
+- **HTML**
+- **PDF**
+
+### Supported Languages
+
+- **French (fr)**
+- **English (en)**
+- **Dutch (nl)**
+- **German (de)**
+
+### Folder Structure
+
+When launched, the tool will create an `Output` folder in the directory where it's located. Inside the `Output` folder, separate folders for each selected export format will be created, with the exported files saved in the respective formats.
+Metadata text files and log will also be located in this folder.
+
+## Example Usage
+
+1. Launch the application.
+2. Go to **Tab 2: Metadata** and click **Export metadata** to retrieve information about the datasets and views.
+3. Switch to **Tab 3: All Views** to export **all views** available in the selected formats and languages. Not recommended.
+4. In **Tab 4: Export selected views**, choose specific views, select formats and languages, and then click **Export views**.
+
+The progress of the export will be displayed in real-time.
+
+## Troubleshooting
+
+- **No progress or export failure**: If the application fails to fetch data from the StatBel API, it will retry up to 5 times with a 3-second delay between each attempt.
+- **File name issues**: The tool automatically sanitizes file names to remove invalid characters that may cause issues on Windows file systems.
+- **Not responding**: The tool may looks like it isn't responding during exporting files.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- StatBel_OpenDatasets.py
+++ StatBel_OpenDatasets.py
@@ -23,54 +23,94 @@ VALID_LANGUAGES = ['fr','en','nl','de']
 # ===============================================================
 from datetime import datetime
 import requests
-import json
+import logging
 import urllib.request
-import ctypes
 import logging
 import sys
 import os
 import time
 from requests.exceptions import ConnectionError, Timeout
-from tkinter import  ttk,filedialog, messagebox
+from tkinter import  ttk,messagebox
 import tkinter as tk
 # ===============================================================
 # Functions
 # ===============================================================
 def sanitize_filename(filename):
-    # Define a list of characters to be replaced or removed
+    """
+    Sanitizes a filename by replacing invalid characters with underscores.
+
+    This function ensures the filename is safe for use on a Windows file system 
+    by replacing characters that are not allowed in Windows filenames with underscores.
+    The invalid characters include: 
+    '<', '>', ':', '"', '/', '\\', '|', '?', and '*'.
+
+    Args:
+        filename (str): The original filename to be sanitized.
+
+    Returns:
+        str: The sanitized filename with invalid characters replaced by underscores.
+    
+    Example:
+        >>> sanitize_filename("my:file|name?.txt")
+        "my_file_name_.txt"
+    """
+    # Define a list of characters to be replaced
     invalid_chars = r'<>:"/\\|?*'  # These are illegal characters in Windows filenames
     sanitized_filename = filename
 
-    # Replace or remove invalid characters
+    # Replace invalid characters
     for char in invalid_chars:
-        sanitized_filename = sanitized_filename.replace(char, '_')  # Replace with '_'    
+        sanitized_filename = sanitized_filename.replace(char, '_')    
     return sanitized_filename
 
 def fetch_data_with_retry(url, retries=5, delay=3):
     attempt = 0
     while attempt < retries:
         try:
-            response = requests.get(url, timeout=10)  # Timeout set to 10 seconds
-            response.raise_for_status()  # Will raise HTTPError for bad responses (4xx or 5xx)
+            response = requests.get(url, timeout=10)  
+            response.raise_for_status() 
             return response.json()  # Assuming JSON response
         except ConnectionError as e:
-            print(f"ConnectionError: {e}. Retrying in {delay} seconds...")
+            logging.error(f"ConnectionError: {e}. Retrying in {delay} seconds...")
         except Timeout as e:
-            print(f"TimeoutError: {e}. Retrying in {delay} seconds...")
+            logging.error(f"TimeoutError: {e}. Retrying in {delay} seconds...")
         except requests.exceptions.HTTPError as e:
-            print(f"HTTPError: {e}. Response code: {e.response.status_code}")
+            logging.error(f"HTTPError: {e}. Response code: {e.response.status_code}")
             break
         except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+            logging.error(f"An unexpected error occurred: {e}")
             break
         
-        time.sleep(delay)  # Wait before retrying
+        time.sleep(delay)  
         attempt += 1
 
-    print("Max retries reached. Could not fetch the data.")
+    logging.error("Max retries reached. Could not fetch the data.")
     return None
+
 # Function to get the file path (works for both Python script and compiled executable)
 def get_file_path(file_name):
+    """
+    Fetch data from a given URL with retry logic in case of network-related errors.
+
+    This function attempts to retrieve data from the specified URL using the `requests.get` method.
+    If the request fails due to a connection error, timeout, or any other exception, it will retry 
+    the request up to the specified number of retries, with a delay between each attempt.
+
+    Parameters:
+    - url (str): The URL from which to fetch data.
+    - retries (int, optional): The maximum number of retry attempts (default is 5).
+    - delay (int, optional): The delay in seconds between each retry attempt (default is 3).
+
+    Returns:
+    - dict: The parsed JSON response from the server if the request is successful.
+    - None: If the request fails after the maximum number of retries or if an HTTP error occurs.
+
+    Raises:
+    - None: All exceptions are handled internally, and None is returned in case of failure.
+
+    Logs:
+    - Logs error messages for each failed attempt and when the retry limit is reached.
+    """
     # If running from a bundled exe, the file will be in same folder as the exe
     if getattr(sys, 'frozen', False):
         # Running as a bundled exe, get the path to the directory of the exe
@@ -80,16 +120,30 @@ def get_file_path(file_name):
         # Running as a Python script
         return os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)    
 
-# Function to update the label and refresh the window
-def update_label(text):
-    label.config(text=text)
-    root.update()  
-# Function to close the window
-def close_window():
-    root.quit()
 
-# Based on json datas
 def write_json_to_txt(txtfile, json,header):
+    """
+    Writes the contents of a JSON-like structure to a text file with a specified format.
+
+    Args:
+        json_data (list of dict): A list of dictionaries containing the data to be written.
+        txtfile (file-like object): The text file object where the data will be written.
+        header (str): A header string to display at the beginning of each section.
+
+    The function processes each dictionary in the input `json_data` list and writes its keys and values 
+    into the provided `txtfile`. If a value is itself a dictionary (nested dictionary), its keys and 
+    values are indented and written accordingly. A separator line (`"-" * 50`) is written after each dictionary 
+    to clearly separate entries. The header is written at the beginning of each entry.
+
+    Example:
+        json_data = [
+            {'name': 'Alice', 'age': 30, 'address': {'city': 'New York', 'zipcode': '10001'}},
+            {'name': 'Bob', 'age': 25, 'address': {'city': 'Los Angeles', 'zipcode': '90001'}}
+        ]
+        with open('output.txt', 'w') as file:
+            write_json_to_txt(json_data, file, 'User Information')
+    """
+    # Based on json datas
     for data in json:
         txtfile.write(f"--- {header} ---\n")
         for key, value in data.items():
@@ -101,7 +155,6 @@ def write_json_to_txt(txtfile, json,header):
                 txtfile.write(f"{key}: {value}\n")
         txtfile.write("-" * 50 + "\n")
 
-
 def create_metadata() :
     # --- Create medatata.txt to inform users
     Metadata_path = os.path.join(outputFolder,'metadata.txt')
@@ -110,8 +163,31 @@ def create_metadata() :
         write_json_to_txt(file, views,'View')
     metadata_lb.config(text=f"Export done ! See {Metadata_path}")
 
-# Function to print selected exporters and languages
+def create_linked_views_metadata() :
+    Metadata_path = os.path.join(outputFolder,'linked_views.txt')
+    with open(Metadata_path, 'w') as file:
+        # Link views to the corresponding data sources and write to the file
+        for data_source in datasources:
+            # Find all views that are linked to the current data source
+            linked_views = [view for view in views if view["dataSourceId"] == data_source["id"]]
+            
+            # Write the data source and its linked views to the file
+            file.write(f"Data Source ID: {data_source['id']}\n")
+            file.write(f"Data Source Name: {data_source['name']}\n")
+            file.write("Linked Views:\n")
+            
+            if linked_views:
+                for view in linked_views:
+                    file.write(f"  - View ID: {view['id']}, View Name: {view['name']}\n")
+            else:
+                file.write("  No linked views.\n")
+            
+            file.write("===================================\n")
+    metadatalk_lb.config(text=f"Results have been written to {Metadata_path}")
+
+
 def export_views() :
+    # Function to print selected exporters and languages
     # Get selected values
     selected_exporters = [VALID_EXPORTERS[i] for i, var in enumerate(exporter_vars) if var.get()]
     selected_languages = [VALID_LANGUAGES[i] for i, var in enumerate(lang_vars) if var.get()]
@@ -139,6 +215,7 @@ def export_views() :
     export_progress_bar['value'] = 0  # Reset the progress bar
     root.update_idletasks()  # Update the window
 
+    error_occurred = False  # Flag to check if any error occurred
     # --- Iterate through each dataset and export it
     for exporter in selected_exporters :
         # Create a folder for each exporter if it doesn't exist
@@ -156,9 +233,11 @@ def export_views() :
                 urllib.request.urlretrieve(url, local_filename)
                 #print(f"File successfully saved to {local_filename}")
             except Exception as e:
-                # Print the error and continue to the next exporter
-                print(f"Error downloading {url}: {e}")
+                # Log the error
+                logging.error(f"Error downloading {url}: {e}")
+                error_occurred = True
                 continue  # Continue with the next exporter
+
             
             # Update Progressbar
             processed_items += 1
@@ -167,9 +246,16 @@ def export_views() :
             root.update_idletasks()  # Update the window to reflect the changes
 
     duration = datetime.now() - start_time
-    export_progress_bar_lb.config(text=f"Export Done! Duration: {duration.days*24 + duration.seconds//3600} hour(s), {(duration.seconds//60)%60} min, {duration.seconds%60} sec")
+    # Ensure progress bar reaches 100% at the end. When errors, it doesn't reach 100%, it's why we force it
+    export_progress_bar['value'] = 100
+    root.update_idletasks()  # Make sure the GUI reflects the final update
 
-# Function to print selected exporters and languages
+    # Update progress label based on whether an error occurred
+    if error_occurred:
+        export_progress_bar_lb.config(text=f"Export Done with errors! Duration: {duration.days*24 + duration.seconds//3600} hour(s), {(duration.seconds//60)%60} min, {duration.seconds%60} sec. Check 'export_errors.log' for details.")
+    else:
+        export_progress_bar_lb.config(text=f"Export Done! Duration: {duration.days*24 + duration.seconds//3600} hour(s), {(duration.seconds//60)%60} min, {duration.seconds%60} sec")
+
 def export_all_views() :
     # --- Iterate through each dataset and export it
     for exporter in VALID_EXPORTERS :
@@ -188,8 +274,8 @@ def export_all_views() :
                 urllib.request.urlretrieve(url, local_filename)
                 #print(f"File successfully saved to {local_filename}")
             except Exception as e:
-                # Print the error and continue to the next exporter
-                print(f"Error downloading {url}: {e}")
+                # Log the error
+                logging.error(f"Error downloading {url}: {e}")
                 continue  # Continue with the next exporter
 
 def update_views_listbox():
@@ -204,6 +290,12 @@ def update_views_listbox():
     for item in sorted_filtered_views:
         views_listbox.insert(tk.END, item['name'])
 
+# Function to show the popup and stop the script
+def show_error_and_exit(message):
+    root = tk.Tk()
+    root.withdraw()  # Hide the root window
+    messagebox.showerror("Error", message)
+    sys.exit(1)  # Stop the script
         
 # ===============================================================
 # Init script : Create Output and get api json
@@ -211,11 +303,17 @@ def update_views_listbox():
 # -- Create Output folder
 outputFolder = get_file_path("Output")
 os.makedirs(outputFolder, exist_ok=True)
+# -- Setup logging configuration
+log_file = os.path.join(outputFolder, 'export_errors.log')
+logging.basicConfig(filename=log_file, level=logging.ERROR, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 # --- Send a GET request to fetch the raw JSON data with all views (to get ids)
 # Datasource are not exportable. Views are. Each views is linked, via ID, to a datasource
 datasources = fetch_data_with_retry(f"{API_URL}/datasources")
 views = fetch_data_with_retry(f"{API_URL}/views")
-
+# if cannont get data : stop script
+if datasources is None or views is None:
+    show_error_and_exit(f"Datasources or Views data is missing. Please check the logfile ({outputFolder})")
 
 # ===============================================================
 # Init Window
@@ -248,17 +346,30 @@ style.configure(
 # ===============================================================
 # Create my tab
 tab0 = ttk.Frame(notebook)
-notebook.add(tab0, text='ReadMe' )
+notebook.add(tab0, text='About' )
 
 # Adding a Text widget to ReadMe_frame
 text_widget_tab0 = tk.Text(tab0, wrap='word' , bg=style.lookup("TFrame", "background"), fg='Black', bd=0)
 # Text
-Text_tab0 = f"""
-Author : Ronaf-Git
+Text_tab0 = """
+    StatBel Open Datasets Tool
 
-How-to :
+    This tool allows you to retrieve and export datasets from the Belgian Statistical Office's StatBel API in various formats.
 
-See https://github.com/Ronaf-git/StatBel_OpenDatasets for more informations
+    Key Features:
+    - Export Metadata: Retrieve information about available datasets.
+    - Export All Views: Download all datasets (all languages) in selected formats (XML, JSON, CSV, XLS, HTML, PDF).
+    - Export Specific Views: 
+        Filtered by Language: French, English, Dutch, or German.
+        Individually selected.
+        In selected format.
+
+    Usage:
+    1. Export Metadata: Click "Export Metadata" to generate a text file with dataset information.
+    2. Export Views: In the "Export Views" tab, select formats and languages, then click "Export Views" to download your data.
+
+    Author: Ronaf-Git
+    For more information, visit: https://github.com/Ronaf-git/StatBel_OpenDatasets
 """
 text_widget_tab0.insert(tk.END,Text_tab0) 
 # Set the Text widget to "disabled" to make it unwritable
@@ -273,14 +384,22 @@ text_widget_tab0.pack(fill="both", expand=True)
 # ==============================================================================================================================
 # Create Tab
 tab1 = ttk.Frame(notebook)
-notebook.add(tab1, text='Metadatas' )
+notebook.add(tab1, text='Metadata' )
 # --- metadata : button and label
 # Create Button 
-metadata_bt = tk.Button(tab1, text="Export metadatas", command=create_metadata)
+metadata_bt = tk.Button(tab1, text="Export metadata", command=create_metadata)
 metadata_bt.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 # Create Label 
-metadata_lb = tk.Label(tab1, text="Export all metadata (Datasource and views) in a texte file", font=("Helvetica", 14, "bold"))
+metadata_lb = tk.Label(tab1, text="Export metadata (Datasources and views) in a texte file", font=("Helvetica", 14, "bold"))
 metadata_lb.grid(row=0, column=0, sticky="nsew", padx=10, pady=5)
+
+# --- relationnal link between datasources and views : button and label
+# Create Button 
+metadatalk_bt = tk.Button(tab1, text="Export relationnal metadata", command=create_linked_views_metadata)
+metadatalk_bt.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
+# Create Label 
+metadatalk_lb = tk.Label(tab1, text="Show relations between datasources and views", font=("Helvetica", 14, "bold"))
+metadatalk_lb.grid(row=2, column=0, sticky="nsew", padx=10, pady=5)
 
 # ===============================================================================================================
 # Tab 2 : All datas
@@ -308,9 +427,6 @@ for i in range(20):
     tab3.grid_rowconfigure(i, weight=1)
 for i in range(0):
     tab3.grid_columnconfigure(i, weight=1)
-
-
-
 
 # --- Exporters : Checkbuttons and label
 # Create Label
@@ -375,8 +491,6 @@ views_listbox.grid(row=0, column=0, sticky="nsew")  # Make the listbox fill the 
 # Configure the frame to expand and fill the space
 views_frame.grid_rowconfigure(0, weight=1)
 views_frame.grid_columnconfigure(0, weight=1)
-
-
 
 # --- export : button, label and progressbarr
 # Create Button 
